f31634cb6c93696ebe1ef73d2c81c23c4c78cc55
thehongbaokhanh <thehongbaokhanh2005@gmail.com>
2026-06-15 13:39:21 +0700

    variable and how to call and work with

--- app.go
+++ app.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	fmt.Print("Please enter the name: ")
-	fmt.Scanln(&name)
+	var name string = "Khánh"
+	name2 := "Khánh"
 	fmt.Println("Hello", name)
+	fmt.Println("Hello", name2)
+	randomUser() 
 }
