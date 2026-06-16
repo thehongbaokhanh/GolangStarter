959e6528f8bdea25b03ff4f4bdee9938e8180e4d
thehongbaokhanh <thehongbaokhanh2005@gmail.com>
2026-06-16 09:12:26 +0700

    exercise with function

--- app.go
+++ app.go
@@ -2,8 +2,46 @@ package main
 
 import "fmt"
 
+// Bài 1:
+// func sum(a int) int {
+// 	result := 0
+// 	if a > 0 {
+// 		result = a + sum(a-1)
+// 	}
+
+// 	return result
+// }
+
+
+
+// Bài 2:
+
+// func fibonacci(count int)  int {
+// 	a := 0
+// 	b := 1
+// 	result := 0
+// 	sumResult := 0
+// 	for i := 0; i < count; i++ {
+// 		result = a + b
+// 		a = b
+// 		b = result
+// 		sumResult = sum(result, sumResult)
+// 		fmt.Println( result)
+// 	}
+// 	return sumResult
+// }
+
+// func sum(a, b int) int {
+// 	result := 0
+// 	result = a + b
+// 	return result
+// }
+
+
 func main() {
-	for i := 0; i < 5; i++ {
-		
-	}
+	var count int
+	fmt.Print("Nhập số lượng phần tử Fibonacci cần tính: ")
+	fmt.Scanln(&count)
+	sumFib := fibonacci(count)
+	fmt.Printf("Tổng của các số Fibonacci từ 0 đến %d là: %d\n", count, sumFib)
 }
