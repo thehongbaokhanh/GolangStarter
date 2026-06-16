eaa2f74dd6d8f4e41096480eb44698088d9a50cb
thehongbaokhanh <thehongbaokhanh2005@gmail.com>
2026-06-16 16:54:41 +0700

    exercise struct

--- app.go
+++ app.go
@@ -2,5 +2,21 @@ package main
 
 import "fmt"
 
+type Rectangle struct {
+	Width  float64  
+	Height float64 
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
 func main() {
+	rect := Rectangle{Width: 5, Height: 3}
+	fmt.Printf("Area: %v\n", rect.Area())
+	fmt.Printf("Perimeter: %v\n", rect.Perimeter())
 }
