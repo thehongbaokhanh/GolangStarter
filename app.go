e820fbcf5e14efc6a354eb79a1e1d92e54e86664
thehongbaokhanh <thehongbaokhanh2005@gmail.com>
2026-06-17 13:57:51 +0700

    demo the project adam eva and an apple

--- app.go
+++ app.go
@@ -2,5 +2,25 @@ package main
 
 import "fmt"
 
+type Human struct {
+	name   string
+	weight float32
+}
+
+type Food struct {
+	weight float32
+	name   string
+	isExist bool
+}
+
+func (h Human) eat(f Food) {
+	weight := h.weight + f.weight
+	fmt.Println(h.name, "eat", f.name)
+	fmt.Println("New weight:", weight)
+}	
+
 func main() {
+	Adam := Human{"Tom", 50}
+	apple := Food{10, "apple", true}
+	Adam.eat(apple)
 }
